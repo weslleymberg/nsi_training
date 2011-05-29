34951333aa7fd86cd5973a2b4895b14c85108b36
weslleymberg <weslleym.lisboa@gmail.com>
2011-05-29 14:49:40 -0300

    Training List

---
+++ training-spec.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from should_dsl import should
+from training import Ball, Square, Rectangle, Person, TV, BankAccount
+
+
+class TestBall(unittest.TestCase):
+
+    def setUp(self):
+        self.a_ball = Ball(5, "red") #Ball(radius, color)
+
+    def it_creates_a_ball(self):
+        self.a_ball |should| be_instance_of(Ball)
+
+    def it_changes_the_size_of_the_ball(self):
+        self.a_ball.size = 7
+        self.a_ball.size |should| be(7)
+
+    def it_changes_and_check_the_color_of_the_ball(self):
+        self.a_ball.color = "blue"
+        self.a_ball.color |should| be("blue")
+
+
+class TestSquare(unittest.TestCase):
+
+    def setUp(self):
+        self.a_square = Square(2) #Square(side_weight)
+
+    def it_creates_a_square(self):
+        self.a_square |should| be_instance_of(Square)
+
+    def it_changes_and_check_the_side_of_the_square(self):
+        self.a_square.side = 3
+        self.a_square.side |should| be(3)
+
+    def it_takes_the_square_of_the_side(self):
+        self.a_square.side = 5
+        self.a_square.square() |should| be(25)
+
+
+class TestRectangle(unittest.TestCase):
+
+    def setUp(self):
+        self.a_rectangle = Rectangle(3, 2) #Rectangle(base, height)
+
+    def it_creates_a_rectangle(self):
+        self.a_rectangle |should| be_instance_of(Rectangle)
+
+    def it_changes_the_side_of_the_rectangle(self):
+        self.a_rectangle.base = 5
+        self.a_rectangle.base |should| be(5)
+        self.a_rectangle.height = 4
+        self.a_rectangle.height |should| be(4)
+
+    def it_takes_the_perimeter_of_the_rectangle(self):
+        self.a_rectangle.base = 6
+        self.a_rectangle.height = 4
+        self.a_rectangle.perimeter() |should| be(20)
+
+    def it_takes_the_area_of_the_rectangle(self):
+        self.a_rectangle.base = 5
+        self.a_rectangle.height = 3
+        self.a_rectangle.area() |should| be(15)
+
+
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        self.a_person = Person(12, 50, 140) #People(age, weight, height in centimeters)
+
+    def it_creates_a_person(self):
+        self.a_person |should| be_instance_of(Person)
+
+    def it_changes_the_weight_of_the_person(self):
+        self.a_person.weight = 45
+        self.a_person.weight |should| be(45)
+        self.a_person.weight = 55
+        self.a_person.weight |should| be(55)
+
+    def it_ages_the_person(self):
+        self.a_person.get_old(13)
+        self.a_person.age |should| be(13)
+        self.a_person.height |should| be(155)
+        self.a_person.get_old(12)
+        self.a_person.age |should| be(13)
+        self.a_person.height |should| be(155)
+
+
+class Testtv(unittest.TestCase):
+
+    def setUp(self):
+        self.a_tv = TV()
+
+    def it_creates_a_TV(self):
+        self.a_tv |should| be_instance_of(TV)
+
+    def it_changes_the_status(self):
+        self.a_tv.status = True
+        self.a_tv.status |should| be(True)
+
+    def it_changes_the_channel(self):
+        self.a_tv.change_channel(4)
+        self.a_tv.current_channel |should| be(4)
+        self.a_tv.change_channel(2)
+        self.a_tv.current_channel |should| be(3)
+        self.a_tv.change_channel(26)
+        self.a_tv.current_channel |should| be(25)
+
+    def it_changes_the_volume(self):
+        self.a_tv.change_volume(3)
+        self.a_tv.current_volume |should| be(3)
+        self.a_tv.change_volume(-1)
+        self.a_tv.current_volume |should| be(0)
+        self.a_tv.change_volume(101)
+        self.a_tv.current_volume |should| be(100)
+
+
+class TestBankAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.an_account = BankAccount("Foo", "1234-5")
+
+    def it_crates_an_account(self):
+        self.an_account |should| be_instance_of(BankAccount)
+
+    def it_takes_the_name_of_the_client(self):
+        self.an_account.client_name |should| be("Foo")
+
+    def it_takes_the_number_of_the_account(self):
+        self.an_account.account_number |should| equal_to("1234-5")
+
+    def it_makes_account_operations(self):
+        self.an_account.deposit(100)
+        self.an_account.average_credit |should| be(100)
+        self.an_account.draw(25)
+        self.an_account.average_credit |should| be(75)
---
+++ training.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Ball(object):
+
+    def __init__(self, size, color):
+        self.size = size
+        self.color = color
+
+
+class Square(object):
+
+    def __init__(self, side):
+        self.side = side
+
+    def square(self):
+        return self.side ** 2
+
+
+class Rectangle(object):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def perimeter(self):
+        return 2 * (self.base + self.height)
+
+    def area(self):
+        return self.base * self.height
+
+
+class Person(object):
+
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+    def get_old(self, new_age):
+        while new_age - self.age > 0:
+            if new_age > self.age:
+                if self.age < 21:
+                    self.height += 15
+                self.age = new_age
+            new_age -= 1
+
+
+class TV(object):
+
+    def __init__(self):
+        self.status = False
+        self.channels = (3, 25)
+        self.current_channel = 3
+        self.volume = (0, 100)
+        self.current_volume = 0
+
+    def change_channel(self, new_channel):
+        self.current_channel = self.channels[1]
+        if new_channel < self.channels[0]:
+            self.current_channel = self.channels[0]
+        if new_channel >= self.channels[0] and new_channel <= self.channels[1]:
+            self.current_channel = new_channel
+
+    def change_volume(self, new_volume):
+        self.current_volume = self.volume[1]
+        if new_volume < self.volume[0]:
+            self.current_volume = self.volume[0]
+        if new_volume >= self.volume[0] and new_volume <= self.volume[1]:
+            self.current_volume = new_volume
+
+
+class BankAccount(object):
+
+    def __init__(self, name, account_number):
+        self.client_name = name
+        self.account_number = account_number
+        self.average_credit = 0
+
+    def deposit(self, value):
+        self.average_credit += value
+
+    def draw(self, value):
+        self.average_credit -= value
